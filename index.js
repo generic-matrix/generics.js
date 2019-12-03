70222d7975171379017a3e475ee63e41482cf2c1
kaustubhb <kaustubhbawa123@gmail.com>
2019-12-03 14:28:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/**
+ * generics.js
+ * https://trygistify.com/generics
+ *
+ * A minimal Deep learning library for the web.
+ *
+ * @version 0.1.0
+ * @date Tue Dec 03 2019 09:51:10 GMT-0500 (Eastern Standard Time)
+ *
+ * @license MIT
+ * The MIT License
+ *
+ * Copyright (c) 2019 generics.js team
+ */
+
 'use strict'
 const  Network =require("./src/ANN.js");
 const  Neuron =require("./src/Neuron.js");
