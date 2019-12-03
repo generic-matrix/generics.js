2abb8661c320f1dabcb938bd9d418064ff0f6cb5
kaustubhb <kaustubhbawa123@gmail.com>
2019-12-03 19:52:31 +0530

    Added docs support

--- src/Utilities.js
+++ src/Utilities.js
@@ -29,11 +29,20 @@ function shuffleArray(array1,array2) {
 	}
 }
 
+/**
+    *  A class which helps as a driver core to se the neural network with ease.
+*/
 class Utilities{
-	
+
 	SIGMOID() { return "sigmoid"; }
 	RELU() { return "relu"; }
 	LEAKY_RELU() { return "leaky_relu"; }
+    
+    /**
+    * Saves the model in a directory. 
+    * @param {Network}  net
+    * @param {string}  Directory: to save the ANN in a directory.
+    */
 	save_model(net,dir){
 		var json={};
 		json.topology=net.topology;
@@ -48,7 +57,7 @@ class Utilities{
 			weight.push(lay);
 		});
 		json.weights=weight;
-		
+
 		try{
 			fs.writeFileSync(dir,JSON.stringify(json), 'utf8');
 			console.log("Saved the model "+dir);
@@ -56,33 +65,54 @@ class Utilities{
 			throw new Error(err);
 		}
 	}
-		
+
+    /**
+    * To train a neural network - Takes the entire batch size
+    * @param {Network}  net
+    * @param {string}  inputs: The x axis of the data set.
+    * @param {string}  output: The y axis of the data set.
+    * @param {string}  count:  The count value.
+    */
 	train(net,inputs,output,count){
-			for(var j=0;j<count;j++){
-					var erro = 0.0;
-					for (var i = 0; i < inputs.length ; i++) {
-							net.setInput(inputs[i]);
-							net.feedForward();
-							net.backPropogate(output[i]);
-							erro = erro + net.getError(output[i]);							
-					}
-					try{
-					process.stdout.cursorTo(0);
-					process.stdout.write("Error: "+erro);
-					}catch(err){
-						console.log("Error: "+erro);
-					}
+		for(var j=0;j<count;j++){
+			var erro = 0.0;
+			for (var i = 0; i < inputs.length ; i++) {
+				net.setInput(inputs[i]);
+				net.feedForward();
+				net.backPropogate(output[i]);
+				erro = erro + net.getError(output[i]);
 			}
-			
+			try{
+				process.stdout.cursorTo(0);
+				process.stdout.write("Error: "+erro);
+			}catch(err){
+				console.log("Error: "+erro);
+			}
 		}
-	
+
+	}
+
+    /**
+    * To get predictions from the neural network.
+    * @param {Network}  net
+    * @param {string}  inputs: The x axis of the data set.
+    * @param {input_array}  input_array: The x axis of the data set.
+    * @returns {array}  result:  The result predicted by the network.
+    */
 	predict(net,input_arr){
 		net.setInput(input_arr);
 		net.feedForward();
 		return net.getTheResults();
-	}	
-	
-	restore_model(dir,use_gpu){
+	}
+
+    /**
+    * To get restore model from the JSON file.
+    * @param {string}  directory: To the JSON file.
+    */
+    
+	restore_model(dir){
+        //,use_gpu --> int he next update.
+        var usr_gpu=null;
 		return new Promise(function(resolve, reject) {
 			fs.readFile(dir, function read(err, data) {
 				if (err) {
@@ -110,11 +140,22 @@ class Utilities{
 			});
 		});
 	}
+    
+     /**
+    * To test the neural network predictions with the expected predictions.
+    * @param {Network}  net : The model object.
+    * @param {array}  x_test_axis : The x_axis as the input for which we need the predictions.
+    * @param {array}  y_test_axis : The y_axis is the output the model is intended to do.
+    * @param {JSON}  json : The JSON must contain the keys "g_x_plots" , "e_x_plots" and "y_plots" all array paramas.
+    * @param {number}  step : A numerical value 
+    * @param {number}  threashold : The threashold value ,if the predicted value is less  than it then success else it's counted as failue.
+    */
+    
 	test(net,x_test_axis,y_test_axis,json,step,threashold){
-			var ctr=0;
-			var success=0;
-			var failure=0;
-			x_test_axis.forEach(function(input_arr){
+		var ctr=0;
+		var success=0;
+		var failure=0;
+		x_test_axis.forEach(function(input_arr){
 			net.setInput(input_arr);
 			net.feedForward();
 			var val1=Math.abs(net.getTheResults()[0]);
@@ -131,51 +172,63 @@ class Utilities{
 			console.log("Given: "+val1+" Expected: "+(val2)+" Difference: "+dif);
 			ctr++;
 		});
-		
+
 		console.log("Correct : "+success+" -- Failed : "+failure);
-			
+
 	}
-	
+
+     /**
+    * To test the neural network predictions with the expected predictions.
+    * @param {Network}  net : The model object.
+    * @param {array}  input : The x_axis as the input for which we need the predictions.
+    * @param {array}  output : The y_axis is the output the model is intended to do.
+    * @param {number}  folds : By what size the data set must be folded ,defins the batch size.
+    * @param {number}  count  : How many times a neural network must be trained .
+    * @param {string}  dir : The  directory where the model must be saved.
+    * @param {number}  threashold : The threashold value ,if the predicted value is less  than it then success else it's counted as failue.
+    * @returns {number}  percent: The percentage bywhich a input data must be divided in a batch size.
+    */
+    
 	perform_k_fold(net,input,output,folds,count,dir,threashold,percent){
 
-			return new Promise(function(resolve, reject) {
-				if(input.length!=output.length){
-					reject("The x axis and the y axis length is not same");
+		return new Promise(function(resolve, reject) {
+			if(input.length!=output.length){
+				reject("The x axis and the y axis length is not same");
+			}
+			if(percent<=0||percent>=100){
+				reject("The split percent is invalid.");
+			}
+
+			var json={};
+			json.title="Metrics";
+			json.x_axis="Steps";
+			json.y_axis="predictions";
+			json.e_x_plots=[];
+			json.g_x_plots=[];
+			json.y_plots=[];
+
+			if(folds==0||folds.length>=input.length){
+				reject("Fold value is invalid.");
+			}else{
+				shuffleArray(input,output);
+				var x_data_set=arr_split(input,folds);
+				var y_data_set=arr_split(output,folds);
+				for(let j=0;j<x_data_set.length;j++){
+					var index=Math.round((percent/100)*x_data_set[j].length);
+					var x_train=x_data_set[j];
+					var y_train=y_data_set[j];
+					var x_test=x_train.splice(0,index);
+					var y_test=y_train.splice(0,index);
+					var util=new Utilities();
+					util.train(net,x_train,y_train,count);
+					util.test(net,x_test,y_test,json,j,threashold);
+					util.save_model(net,dir);
 				}
-				if(percent<=0||percent>=100){
-					reject("The split percent is invalid.");
-				}
-				
-				var json={};
-				json.title="Metrics";
-				json.x_axis="Steps";
-				json.y_axis="predictions";
-				json.e_x_plots=[];
-				json.g_x_plots=[];
-				json.y_plots=[];
-					
-				if(folds==0||folds.length>=input.length){
-					reject("Fold value is invalid.");
-				}else{
-					shuffleArray(input,output);
-					var x_data_set=arr_split(input,folds);
-					var y_data_set=arr_split(output,folds);
-					for(let j=0;j<x_data_set.length;j++){
-						var index=Math.round((percent/100)*x_data_set[j].length);
-						var x_train=x_data_set[j];
-						var y_train=y_data_set[j];
-						var x_test=x_train.splice(0,index);
-						var y_test=y_train.splice(0,index);
-						var util=new Utilities();
-						util.train(net,x_train,y_train,count);
-						util.test(net,x_test,y_test,json,j,threashold);
-						util.save_model(net,dir);
-					}
-				
-				}
-				resolve(json);
-		  });
+
+			}
+			resolve(json);
+		});
 	}
-		
+
 }
 module.exports = Utilities
